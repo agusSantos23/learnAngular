23c1c0fe455f607d2caaf981ec86dfb514bd7f83
agusSantos23 <131393036+agusSantos23@users.noreply.github.com>
2025-01-07 11:20:21 +0100

    Rutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ProductCardComponent, HeaderComponent],
+  imports: [RouterOutlet, ProductCardComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
