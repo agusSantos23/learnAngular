98410ba436c696db8e01474f3456b0c17d7c85d3
Agustin <agustindelossantos023@gmail.com>
2025-04-25 09:28:56 +0200

    ADD wraper funciona con div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { HeaderComponent } from "./components/header/header.component";
+import { WrapperComponent } from "./wrapper/wrapper.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductCardComponent, HeaderComponent],
+  imports: [RouterOutlet, ProductCardComponent, HeaderComponent, WrapperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/wrapper/wrapper.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wrapper',
+  imports: [],
+  templateUrl: './wrapper.component.html',
+  styleUrl: './wrapper.component.css'
+})
+export class WrapperComponent {
+
+}
